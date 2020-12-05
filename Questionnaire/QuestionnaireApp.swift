fcb38e141d8ac0d86a10586cd6a6a65aa683c7c3
Mike Rapadas <mike@mrap.me>
2020-12-05 13:55:18 -0800

    Initial Commit

---
+++ Questionnaire/QuestionnaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionnaireApp.swift
+//  Questionnaire
+//
+//  Created by Mike Rapadas on 12/5/20.
+//
+
+import SwiftUI
+
+@main
+struct QuestionnaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
