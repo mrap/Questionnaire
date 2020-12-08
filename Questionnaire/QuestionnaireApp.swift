9754b6e4a5417f377846b3ced809148ae294e1de
Mike Rapadas <mike@mrap.me>
2020-12-07 21:29:22 -0800

    Dirty progress commit.  Will squash into next one

--- Questionnaire/QuestionnaireApp.swift
+++ Questionnaire/QuestionnaireApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct QuestionnaireApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            QuestionnaireView().environmentObject(UserData())
         }
     }
 }
---
+++ Questionnaire/Views/FieldTextInput.swift
@@ -0,0 +1,23 @@
+//
+//  FieldTextInput.swift
+//  Questionnaire
+//
+//  Created by Mike Rapadas on 12/7/20.
+//
+
+import SwiftUI
+
+struct FieldTextInput: View {
+    var placeholder: String
+    @Binding var value: String
+
+    var body: some View {
+        TextField(placeholder, text: $value)
+    }
+}
+
+struct FieldTextInput_Previews: PreviewProvider {
+    static var previews: some View {
+        FieldTextInput(placeholder: "My Value", value: .constant("Value"))
+    }
+}
